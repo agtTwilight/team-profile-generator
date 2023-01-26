4635ac2b51764ac3ee2abb1d396b4a98ab69f9f6
agtTwilight <lukasmacmillen@gmail.com>
2023-01-26 13:36:32 -0800

    removed team.html, removed unnecessary constants

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 // Import all require things
 const inquirer = require('inquirer');
 const fs = require('fs');
-const Employee = require('./lib/Employee');
-const Engineer = require('./lib/Engineer');
-const Intern = require('./lib/Intern');
-const Manager = require('./lib/Manager');
 const util = require("util");
 const addTeamMember = require("./util/addTeamMember")
 const constructTeamMember = require('./util/constructTeamMember')
